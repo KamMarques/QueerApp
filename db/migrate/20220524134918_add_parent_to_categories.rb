08c33022e822db5e3637027d801567fda6db9fc5
Camille Marques <marquescamille@gmail.com>
2022-06-01 14:17:43 +0200

    migration de la colonne parent(categorie_id) routes et controllers items et reviews

--- db/migrate/20220524134918_add_parent_to_categories.rb
+++ db/migrate/20220524134918_add_parent_to_categories.rb
@@ -1,5 +1,5 @@
 class AddParentToCategories < ActiveRecord::Migration[6.1]
   def change
-    add_reference :categories, :parent, null: false, foreign_key: true
+    add_reference :categories, :category, null: false, foreign_key: true
   end
 end
