5cee8141d0154d9ca2f32430cbb0300925d4fef6
Camille Marques <marquescamille@gmail.com>
2022-06-07 19:26:23 +0200

    seeds controllers tout debut des views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_134918) do
+ActiveRecord::Schema.define(version: 2022_06_01_134020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_134918) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
-    t.index ["category_id"] , name: "index_categories_on_category_id"
+    t.index ["category_id"], name: "index_categories_on_category_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_134918) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.destroy_all
+Category.destroy_all
+User.destroy_all
+
+user1 = User.create!(nickname: "Kam", email: "test@test.com", password: "123456")
+
+category1 =  Category.create!(label: "Culture", category_id: "nil")
+category2 = Category.create!(label: "Lieux", category_id: "nil")
+category3 = Category.create!(label: "Santé et bien-être", category_id: "nil")
+category4 = Category.create!(label: "Sport", category_id: "nil")
+category5 = Category.create!(label: "Pratique", category_id: "nil")
+category6 = Category.create!(label: "Events", category_id: "category1_id")
+category7 = Category.create!(label: "Medias", category_id: "category1_id")
+category8 = Category.create!(label: "Concerts", category_id:"category6_id")
+category9 = Category.create!(label: "Performances", category_id: "category6_id")
+category10 = Category.create!(label: "Expositions", category_id: "category6_id")
+category11 = Category.create!(label: "Lectures", category_id: "category6_id")
+category12 = Category.create!(label: "Politique", category_id: "category6_id")
+category13 = Category.create!(label: "Films", category_id: "category7_id")
+category14 = Category.create!(label: "Livres", category_id: "category7_id")
+category15 = Category.create!(label: "Musique", category_id: "category7_id")
+category16 = Category.create!(label: "Bars", category_id: "category2_id")
+category17 = Category.create!(label: "Restaurants", category_id: "category2_id")
+category18 = Category.create!(label: "Librairies", category_id: "category2_id")
+category19 = Category.create!(label: "Associations", category_id: "category2_id")
+category20 = Category.create!(label: "Medecins/Practiciens", category_id: "category3_id")
+category21 = Category.create!(label: "Associations", category_id: "category3_id")
+category22 = Category.create!(label: "Conseils", category_id: "category3_id")
+category23 = Category.create!(label: "Coiffeurs", category_id: "category3_id")
+category24 = Category.create!(label: "Clubs de sport", category_id: "category4_id")
+category25 = Category.create!(label: "Profs", category_id: "category4_id")
+category26 = Category.create!(label: "Cours", category_id: "category4_id")
+category27 = Category.create!(label: "Associations", category_id: "category4_id")
+category28 = Category.create!(label: "Plombiers", category_id: "category5_id")
+
+item1 = Item.create!(name: "Concert Bikini Kill", description: "Bikini Kill est un groupe de punk rock américain originaire d'Olympia, État de Washington. Formé en octobre 1990, le groupe est considéré comme précurseur du mouvement riot grrrl", address: "Trabendo, Paris", price: "30€", date_start: "08/06/2022")
