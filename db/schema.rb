4f23dc5acb7bf5373b8fac1570b233a046191a3c
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-06-22 09:38:38 +0200

    merging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_131344) do
+ActiveRecord::Schema.define(version: 2022_05_24_130918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,16 +37,6 @@ ActiveRecord::Schema.define(version: 2022_05_24_131344) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text "comment"
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_reviews_on_item_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,6 +51,4 @@ ActiveRecord::Schema.define(version: 2022_05_24_131344) do
 
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
-  add_foreign_key "reviews", "items"
-  add_foreign_key "reviews", "users"
 end
